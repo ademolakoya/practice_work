fd72f58c8d62acdd029a3b656232a5fa1ccbd272
ademolakoya <ademolakoya20@gmail.com>
2025-05-25 21:11:05 +0100

    Create my-module.py
    function for the work

---
+++ my-module.py
@@ -0,0 +1,29 @@
+def reverse_text(text):
+    """
+    Reverses the input text string.
+    
+    Parameters:
+        text (str): The string to reverse.
+        
+    Returns:
+        str: The reversed string.
+    """
+    return text[::-1]
+
+
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+    
+    Parameters:
+        a (float or int): First number.
+        b (float or int): Second number.
+        
+    Returns:
+        float or int: The sum of a and b.
+    """
+    return a + b
+
+
+print(reverse_text("ade"))
+print(add_numbers(3,5))
